3eeae2304d56a55be88a6615198b2a3654d1596f
Saif Hossain <atonlymesaif@gmail.com>
2021-03-06 15:53:26 +0600

    dx pad movement fully completed

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const canvas = document.querySelector('canvas')
 let dxPad = document.getElementById('dx-pad')
+let dxPadPosition = 0
+
+const bodyWidth = document.querySelector('body').clientWidth
 
 document.addEventListener('mousemove', (event) => {
-    console.log(event.clientX);
+	const mousePosition = parseInt(event.clientX) - 100
 
-    dxPad.style.left = `${event.clientX}px`;
+	if (mousePosition < 0) {
+		dxPadPosition = 0
+	} else if (mousePosition > bodyWidth - 200) {
+		dxPadPosition = bodyWidth - 200
+	} else {
+		dxPadPosition = mousePosition
+	}
+
+	dxPad.style.left = `${dxPadPosition}px`
 })
