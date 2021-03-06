de78f60253a89b86e54b7ee1d8bb58dcf187b354
Saif Hossain <atonlymesaif@gmail.com>
2021-03-06 23:15:34 +0600

    click to start event added

--- movingBall.js
+++ movingBall.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas')
 const windowHeight = window.innerHeight - 21
 const windowWidth = window.innerWidth - 1
+let isStarted = false
 
 canvas.height = windowHeight
 canvas.width = windowWidth
@@ -12,6 +13,10 @@ let y = windowHeight / 2
 let vx = 4
 let vy = 4
 let radius = 10
+
+document.addEventListener('click', (e) => {
+	isStarted = true
+})
 
 move()
 
@@ -25,26 +30,28 @@ function move() {
 	context.arc(x, y, radius, 0, Math.PI * 2, false)
 	context.stroke()
 
-	if (radius + x > windowWidth) {
-		vx = -vx
+	if (isStarted) {
+		if (radius + x > windowWidth) {
+			vx = -vx
+		}
+	
+		if (x - radius < 0) {
+			vx = -vx
+		}
+	
+		if (
+			y + radius > windowHeight &&
+			x >= dxPadPosition &&
+			x <= dxPadPosition + 200
+		) {
+			vy = -vy
+		} 
+	
+		if (y - radius < 0) {
+			vy = -vy
+		}
+	
+		x = x + vx
+		y = y + vy
 	}
-
-	if (x - radius < 0) {
-		vx = -vx
-	}
-
-	if (
-		y + radius > windowHeight &&
-		x >= dxPadPosition &&
-		x <= dxPadPosition + 200
-	) {
-		vy = -vy
-	}
-
-	if (y - radius < 0) {
-		vy = -vy
-	}
-
-	x = x + vx
-	y = y + vy
 }
