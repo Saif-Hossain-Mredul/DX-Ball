5a4c4b5bd7f4be943e4fdea9b3dd133d46e49452
Saif Hossain <atonlymesaif@gmail.com>
2021-03-06 16:18:21 +0600

    move function added

--- movingBall.js
+++ movingBall.js
@@ -1,8 +1,26 @@
-const canvas = document.querySelector('canvas') 
+const canvas = document.querySelector('canvas')
 const windowHeight = window.innerHeight - 21
-const windowWidth = window.innerWidth -1 
+const windowWidth = window.innerWidth - 1
 
 canvas.height = windowHeight
 canvas.width = windowWidth
 
-console.log(`${windowHeight} ${windowWidth}`)
+let context = canvas.getContext('2d')
+
+let x = windowWidth / 2
+let y = windowHeight / 2
+let vx = 4
+let vy = 4
+let radius = 20
+
+move()
+
+function move() {
+	requestAnimationFrame(move)
+
+	context.clearRect(0, 0, innerWidth, innerWidth)
+
+	context.beginPath()
+	context.strokeStyle = 'black'
+	context.arc(x, y, radius, 0, Math.PI * 2, false)
+}
