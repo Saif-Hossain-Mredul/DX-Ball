756037fa3b8bcdd5ba556fdf1717ccca6d67c0ac
Saif Hossain <atonlymesaif@gmail.com>
2021-03-07 00:52:46 +0600

    reformatted

--- movingBall.js
+++ movingBall.js
@@ -11,35 +11,36 @@ let context = canvas.getContext('2d')
 let x = dxPadPosition
 let y = windowHeight - 10.5
 let vx = 0
-let vy = 0 
+let vy = 0
 let radius = 10
 const speed = 5
 
 // This function is for generate vy auto
-function generateVY(hypotenuse, base) {
-    let slope = 0.95
 
-    return Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow((base * slope), 2))
+function generateVY(hypotenuse, base) {
+	let slope = 0.95
+
+	return Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(base * slope, 2))
 }
 
-
 document.addEventListener('click', (e) => {
-
 	// Here if the game has started or not is being checked
 	// If its only for first time, then it will auto generate the game with a
 	// arbitrary direction and then the flag variable is switched to false
-	if(!isStarted) {
-		vx = (Math.random() * (3) + 1)
+
+	if (!isStarted) {
+		vx = Math.random() * 3 + 1
 		vy = generateVY(speed, vx)
 
 		const randomNum = Math.ceil(Math.random() * 10)
-		randomNum > 5 ? vx = -vx : vx = vx;
+		randomNum > 5 ? (vx = -vx) : (vx = vx)
 	}
-	isStarted = true 
+	isStarted = true
 })
 
 move()
 
+// This function is for the movement of the ball
 function move() {
 	requestAnimationFrame(move)
 
@@ -54,23 +55,23 @@ function move() {
 		if (radius + x > windowWidth) {
 			vx = -vx
 		}
-	
+
 		if (x - radius < 0) {
 			vx = -vx
 		}
-	
+
 		if (
 			y + radius > windowHeight &&
 			x >= dxPadPosition &&
 			x <= dxPadPosition + 200
 		) {
 			vy = -vy
-		} 
-	
+		}
+
 		if (y - radius < 0) {
 			vy = -vy
 		}
-	
+
 		x = x + vx
 		y = y + vy
 	} else {
